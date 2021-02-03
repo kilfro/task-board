073905bdd6e71a34b0b3df17c2fac757276b33a2
Kirill Frolov <frolov.cyrill@gmail.com>
2021-02-03 20:31:46 +0300

    dragEndHandler first implementation

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,13 +2,47 @@ import React from 'react'
 import {DragDropContext} from 'react-beautiful-dnd'
 import List from './List'
 
-const dragEndHandler = () => {
-}
+const Board = ({tasks, setTasks}) => {
+    const move = (source, destination, draggableId) => {
+        let destList = [...tasks[destination.droppableId]]
+        let sourceList = [...tasks[source.droppableId]]
+        const movedTask = source.find(t => t.id === draggableId)
 
-const Board = ({tasks, setTasks}) => (
-    <DragDropContext onDragEnd={dragEndHandler}>
-        {Object.keys(tasks).map(listName => <List key={listName} droppableId={listName} tasks={tasks[listName]}/>)}
-    </DragDropContext>
-)
+        sourceList = sourceList.filter(t => t.id !== draggableId)
+        if (destination.index === 0) {
+            destList = [movedTask, ...destList]
+        } else if (destination.index === destList.length - 1) {
+            destList = [...destList, movedTask]
+        } else {
+            const sliceOne = destList.slice(0, destination.index)
+            const sliceTwo = destList.slice(destination.index)
+            destList = [...sliceOne, movedTask, ...sliceTwo]
+        }
+
+        const newState = {
+            ...tasks,
+            [source.droppableId]: sourceList,
+            [destination.droppableId]: destList,
+        }
+
+        setTasks(newState)
+    }
+
+    const dragEndHandler = ({source, destination, draggableId}) => {
+        if (!destination) {
+            return
+        }
+
+        if (source.droppableId !== destination.droppableId || source.index !== destination.index) {
+            move(source, destination, draggableId)
+        }
+    }
+
+    return (
+        <DragDropContext onDragEnd={dragEndHandler}>
+            {Object.keys(tasks).map(listName => <List key={listName} droppableId={listName} tasks={tasks[listName]}/>)}
+        </DragDropContext>
+    )
+}
 
 export default Board
